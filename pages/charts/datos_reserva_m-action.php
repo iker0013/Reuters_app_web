9fda3ad2e89d323fb43a4fa2be4a5b1b377a2b83
iker0013 <54087591+iker0013@users.noreply.github.com>
2020-05-09 16:47:02 -0500

    Create datos_reserva_m-action.php

---
+++ pages/charts/datos_reserva_m-action.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Reuters | Hot Desk</title>
+  
+</head>
+<body class="hold-transition login-page">
+    <?php
+        session_start();
+        $num_reserv = htmlspecialchars($_POST['n_reserva']);
+        $fecha = htmlspecialchars($_POST['fecha']);
+        $n_desk = htmlspecialchars($_POST['n_desk']);
+        $hora_i = htmlspecialchars($_POST['hora_i']);
+        $hora_s = htmlspecialchars($_POST['hora_s']);
+        $cadena = $num_reserv.",".$fecha.",".$n_desk.",".$hora_i.",".$hora_s;
+        echo $cadena;
+        $hi= substr($hora_i,0,2);
+        $hf= substr($hora_s,0,2);
+        if(intval($hi)>=intval($hf)){
+            echo "<script>window.alert('La hora de Check In debe ser menor que la de Check out');
+			        location.href='./modificar_reserva.php';
+                </script> ";
+		}else{
+            try{
+                //URL para mandar la infromación al API para que regrese la verificación
+                //$url = 'http://localhost:60863/api/modificar_reservacion/'.base64_encode($cadena);
+            
+                $contents = file_get_contents($url);
+                //Separar los valores de la cadena obtenida
+                $array = explode(",", $contents);
+                $cod = explode(":", $array[0]);
+                $cod= substr($cod[1],1,-1);
+                //Validar usario existente
+                if($cod=="1"){
+                    echo "<script>window.alert('Reserva modificada con exito');
+			                location.href='./modificar_reserva.php';
+                        </script> ";
+		        }else{
+                    echo "<script>window.alert('Error :".$contents."');
+			                location.href='./modificar_reserva.php';
+                        </script> ";
+		        }
+                
+		    }catch(PDOException $e){
+                echo "<script>window.alert('Error :".$e."');
+			            location.href='./modificar_reserva.php';
+                    </script> ";
+		    }
+        }
+        
+
+    ?>
+</body>
+</html>
