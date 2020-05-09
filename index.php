3ea1bed486bd36bf419bb50316ebd94dfeb3449c
iker0013 <54087591+iker0013@users.noreply.github.com>
2020-05-09 16:37:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -27,17 +27,37 @@
   <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!--Nav bar-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="dist/css/nav_bar.css">
+
+
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
+  <div class="topnav" id="nav_mobile">
+    <div id="myLinks">
+        <a href="pages/charts/modificar_reserva.php">Modificar Reserva</a>
+        <a href="pages/charts/reservar.php">Reservar</a>
+        <a href="./index.php">Hogar</a>
+    </div>
+    <a href="javascript:void(0);" class="icon" onclick="navBar()">
+      <i class="fa fa-bars"></i>
+    </a>
+  </div>
   <div class="wrapper">
     <?php
      //Variables from login
-      session_start();
-      
+     session_start();
+     if(!isset($_SESSION["nombre_emp"])){
+		echo "<script> 
+			alert('No estas autorizado para ver esta pagina');
+			location.href='pages/examples/login.php';
+			</script>";
+	 }
     ?>
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4" id="aside1">
       <!-- Brand Logo -->
       <a href="" class="brand-link">
         <img src="https://globalriskinsights.com/wp-content/uploads/2016/03/Reuters-Logo.jpg" alt="AdminLTE Logo"
@@ -89,16 +109,17 @@
                     <p>Modificar reservación</p>
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a href="pages/charts/inline.php" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Cancelar reservación</p>
-                  </a>
-                </li>
               </ul>
             </li>
           </ul>
         </nav>
+
+        <div class="user-panel mt-5 pb-3 mb-3 d-flex">
+          <div class="info">
+            <a href="pages/examples/login.php" class="nav-link">Log out</a>
+          </div>
+        </div>
+
         <!-- /.sidebar-menu -->
       </div>
       <!-- /.sidebar -->
@@ -121,7 +142,19 @@
         </div><!-- /.container-fluid -->
       </div>
       <!-- /.content-header -->
-
+      <script>
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+          document.getElementById("aside1").style.display="none";
+          function navBar() {
+          var x = document.getElementById("myLinks");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+          }
+        }
+      </script>
       <!-- Main content -->
       <section class="content">
         <div class="container-fluid">
@@ -219,7 +252,8 @@
             <section class="col-lg-7 connectedSortable">
               <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">Usuarios activos</h3>
+                  <h3 class="card-title">Usuarios activos</h3><br>
+                  <input type="text" id="usuario_nombre" onkeyup="busarUsuario()" placeholder="Buscar usuario.." title="Escribe el nombre">
                 </div>
                 <?php
                 $url = 'http://localhost:60863/api/datos_dashboard_user/5';
@@ -233,36 +267,65 @@
                   $rows = sizeof($array);
                   ?>
                     <!-- /.card-header -->
-                    <div class="card-body p-0">
-                      <ul class="products-list product-list-in-card pl-2 pr-2">
-                        <?php $n=1;
-                          for($i=0; $i<$rows-1; $i++): 
-                            $innerArr = explode(",", $array[$i]);
-                            ?>
-                            <li class="item">
-                              <div class="product-info">
-                                <a href="javascript:void(0)" class="product-title"><?php echo substr($innerArr[$n],7)?>
-                                  <span class="badge badge-success float-right"><?php echo substr($innerArr[$n+2],13)?></span></a>
-                                <span class="product-description">
-                                  <?php echo substr($innerArr[$n+1],5)?>
-                                </span>
-                              </div>
-                            </li>
-                        <?php $n=2; //The first array is shorter than the others so the value must go up
-                        endfor; ?>
-                      </ul>
-                    </div>
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-striped table-valign-middle" id="tabla_usuarios">
+                          <thead>
+                            <tr>
+                              <th>Nombre</th>
+                              <th>Area</th>
+                              <th>Hot desk</th>
+                            </tr>
+                            <tbody>
+                              <?php $n=1; 
+                                for($i=0; $i<$rows-1; $i++): 
+                                  $innerArr = explode(",", $array[$i]);
+                                ?>
+                                <tr>
+                                  <td><?php echo substr($innerArr[$n],7)?></td>
+                                  <td><?php echo substr($innerArr[$n+1],5)?></td>
+                                  <td><span class="badge badge-success float-left"><?php echo substr($innerArr[$n+2],10)?></span></td>
+                                </tr>
+                              <?php $n=2; //The first array is shorter than the others so the value must go up
+                              endfor; ?>
+                            </tbody>
+                          </thead>
+                        </table>
+                      </div>
+                      
                   <?php
                 }else{
                   echo "&nbsp;&nbsp; No hay usuarios en la oficina";
 				}
                 ?>
+                <!-- Script buscar usuario en tabla usuarios -->
+                <script>
+                    function busarUsuario() {
+                        var input, filter, table, tr, td, i, txtValue;
+                        input = document.getElementById("usuario_nombre");
+                        filter = input.value.toUpperCase();
+                        table = document.getElementById("tabla_usuarios");
+                        tr = table.getElementsByTagName("tr");
+                        for (i = 0; i < tr.length; i++) {
+                            td = tr[i].getElementsByTagName("td")[0];
+                            if (td) {
+                                txtValue = td.textContent || td.innerText;
+                                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                                    tr[i].style.display = "";
+                                } else {
+                                    tr[i].style.display = "none";
+                                }
+                            }       
+                        }
+                    }
+                </script>
+
                 <!-- /.card-body -->
                 <!-- /.card-footer -->
               </div>
             </section>
             <!-- /.Left col -->
             <!-- right col (We are only adding the ID to make the widgets sortable)-->
+            
             <section class="col-lg-5 connectedSortable">
 
               <div class="card">
@@ -286,8 +349,8 @@
                             <tr>
                               <th>Número de Hot Desk</th>
                               <th>Fecha</th>
-                              <th>Hora Inicio</th>
-                              <th>Hora Fin</th>
+                              <th>Hora Check In</th>
+                              <th>Hora Check Out</th>
                             </tr>
                             <tbody>
                               <?php $n=2; 
