8ec4a3251bc6454955148221a4e37fbbf292a5f4
iker0013 <54087591+iker0013@users.noreply.github.com>
2020-05-09 17:01:15 -0500

    Create reservar_a-action.php

---
+++ pages/charts/reservar_a-action.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Reuters | Hot Desk</title>
+  
+</head>
+<body class="hold-transition login-page">
+    <?php
+        session_start();
+        $fecha = htmlspecialchars($_POST['fecha_a']);
+        $hora_i = htmlspecialchars($_POST['hora_a_i']);
+        $hora_s = htmlspecialchars($_POST['hora_a_s']);
+        $cadena = $fecha.",".$_SESSION["numero_emp"].",".$hora_i.",".$hora_s;
+        $hi= substr($hora_i,0,2);
+        $hf= substr($hora_s,0,2);
+        if(intval($hi)>=intval($hf)){
+            echo "<script>window.alert('La hora de Check In debe ser menor que la de Check out');
+			        location.href='./reservar.php';
+                </script> ";
+		}else{
+           try{
+                //URL para mandar la infromación al API para que regrese la verificación
+                $url = 'http://localhost:60863/api/reserva_automatica/'.base64_encode($cadena);
+            
+                $contents = file_get_contents($url);
+                //Separar los valores de la cadena obtenida
+                $array = explode(",", $contents);
+                $cod = explode(":", $array[0]);
+                $cod= substr($cod[1],1,-1);
+                //Validar usario existente
+                if($cod=="1"){
+                    echo "<script>window.alert('Reserva realizada');
+			                location.href='./reservar.php';
+                        </script> ";
+		        }else{
+                    echo "<script>window.alert('Error :".$contents."');
+			                location.href='./reservar.php';
+                        </script> ";
+		        }
+		    }catch(PDOException $e){
+                 echo "<script>window.alert('Error :".$e."');
+			            location.href='./reservar.php';
+                    </script> ";
+		    } 
+        }
+        
+
+    ?>
+</body>
+</html>
